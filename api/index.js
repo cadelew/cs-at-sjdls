b8fb8ce949f822ec07615352b9a6666a30d57bb7
compscibear <cadelewis2024@gmail.com>
2024-07-12 10:31:08 -0700

    create user model

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
  /* if MongoDB is connected, then prints "Connected to MongoDB" */
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB");
 })
+
 /* if MongoDB is not connected, there is an error message */
 .catch((err) => {
     console.log(err)
