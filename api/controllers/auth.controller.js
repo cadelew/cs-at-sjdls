ad234acbe0b71378dfe8d5591b5be7b11b7301ff
compscibear <cadelewis2024@gmail.com>
2024-07-23 13:04:38 -0700

    add redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export const signin = async (req, res, next) => {
         const validUser = await User.findOne({ email });
         if (!validUser) return next(errorHandler(404, 'User Not Found'));
         const validPassword = bcrpytjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(401, 'Invalid Credentials');
+        if (!validPassword) return next(errorHandler(401, 'Invalid Credentials'));
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: hashedPassword, ...rest } = validUser._doc;
         const expiryDate = new Date(Date.now() + 3600000);
