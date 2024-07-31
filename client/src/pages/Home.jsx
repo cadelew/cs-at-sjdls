7777476f61106a3248326dd5dba433358a53e001
compscibear <cadelewis2024@gmail.com>
2024-07-31 01:27:03 -0700

    create home text filler

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4'>CS @ SJDLS</h1>
+      <p>This is a resource to help in ap tests</p>
+    </div>
+  );
 }
