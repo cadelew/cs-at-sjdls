fcce483d84e9896cb3eee5a669c5e9283f457263
compscibear <cadelewis2024@gmail.com>
2025-10-02 00:30:24 -0700

    changed api routes to be more direct, initialized controller files, and completed quiz.controller.js with sorting featurers

---
+++ api/controllers/quiz.controller.js
@@ -0,0 +1,30 @@
+import Quiz from '../models/quiz.model.js';
+import { errorHandler } from '../utils/error.js';
+
+
+export const getQuizzes = async (req, res, next) => {
+    const { sortBy, order } = req.query;
+    const allowedSortBy = ['topic', 'difficulty', 'timeLimit', 'totalQuestions', 'title'];
+    const sortField = allowedSortFields.includes(sortBy) ? sortBy : 'title';
+    const sortOrder = order === 'asc' ? 1 : -1;
+    
+    try {
+        const quizzes = await Quiz.find({ isActive: true }).sort({ [sortField]: sortOrder });
+        res.status(200).json(quizzes);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getQuiz = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const quiz = await Quiz.findById(id);
+        if (!quiz) {
+            return next(errorHandler(404, 'Quiz not found'));
+        }
+        res.status(200).json(quiz);
+    } catch (error) {
+        next(error);
+    }
+}
